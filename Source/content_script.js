774513d5e3ff7325b7d3500a0903ca304b557db9
Dan Marchand <dan.marchand@gmail.com>
2013-12-09 18:28:55 -0500

    Bitcoin to Buttcoin fork

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bBitcoin\b/g, "Buttcoin");
+	v = v.replace(/\bbitcoin\b/g, "buttcoin");
 	
 	textNode.nodeValue = v;
 }
